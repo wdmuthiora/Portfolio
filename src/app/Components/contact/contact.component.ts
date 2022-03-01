02297061203d5e7af5007f303f3e48edb5b11347
wdmuthiora <wdmuthiora@gmail.com>
2022-03-01 11:28:14 +0300

    Imported FormBulder class to Contact me component, and injected FormBuilder Service dependency by adding it to the component's constructor.

--- src/app/Components/contact/contact.component.ts
+++ src/app/Components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private builder:FormBuilder) { }
 
   ngOnInit(): void {
   }
